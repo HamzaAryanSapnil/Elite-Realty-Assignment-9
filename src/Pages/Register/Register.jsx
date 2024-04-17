3dd51dc128308b8ca60e579a06ff389e8a24c1f1
HamzaAryanSapnil <139946462+HamzaAryanSapnil@users.noreply.github.com>
2024-04-17 21:13:40 +0600

    password show button done

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,11 +1,14 @@
 import { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
 import { AuthContext } from "../Provider/AuthProvider";
-import { Link,  useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import { IoEye } from "react-icons/io5";
+import { IoEyeOff } from "react-icons/io5";
 
 const Register = () => {
   const [regError, setRegError] = useState("");
+  const [showPass, setShowPass] = useState(false)
   const { createUser, updateUserProfile } = useContext(AuthContext);
   const navigate = useNavigate();
   const {
@@ -17,16 +20,16 @@ const Register = () => {
   } = useForm();
 
   if (errors?.email) {
-    toast.error(errors.email?.message)
+    toast.error(errors.email?.message);
   }
   if (errors?.password) {
-    toast.error(errors.password?.message)
+    toast.error(errors.password?.message);
   }
   if (errors?.firstName) {
-    toast.error(errors.firstName?.message)
+    toast.error(errors.firstName?.message);
   }
   if (errors?.lastName) {
-    toast.error(errors.lastName?.message)
+    toast.error(errors.lastName?.message);
   }
 
   const onSubmit = (data) => {
@@ -43,19 +46,18 @@ const Register = () => {
         const name = `${data.firstName} ${data.lastName}`;
         const photoUrl = `${data.photoUrl}`;
         updateUserProfile(name, photoUrl)
-        .then(result => {
-          console.log("user profile", result);
-          navigate("/");
-        })
-        .catch((error) => console.log(error));
-        
+          .then((result) => {
+            console.log("user profile", result);
+            navigate("/");
+          })
+          .catch((error) => console.log(error));
       })
       .catch((error) => {
         setRegError(error.message);
         resetField("email");
         resetField("password");
         toast.error(error.message);
-        console.log(error)
+        console.log(error);
       });
   };
 
@@ -81,15 +83,15 @@ const Register = () => {
                   type="text"
                   placeholder="Enter your First name"
                   className="input input-bordered"
-                  {...register("firstName", { required: {
-                    value: true,
-                    message: "First Name field is required"
-                  } })}
+                  {...register("firstName", {
+                    required: {
+                      value: true,
+                      message: "First Name field is required",
+                    },
+                  })}
                 />
                 {errors.firstName && (
-                  
-                  <p className="text-red-500" >{errors.firstName?.message}</p>
-                  
+                  <p className="text-red-500">{errors.firstName?.message}</p>
                 )}
               </div>
               <div className="form-control">
@@ -100,13 +102,15 @@ const Register = () => {
                   type="text"
                   placeholder="Enter your Last name"
                   className="input input-bordered"
-                  {...register("lastName", { required: {
-                    value: true,
-                    message: "Last Name field is required"
-                  } })}
+                  {...register("lastName", {
+                    required: {
+                      value: true,
+                      message: "Last Name field is required",
+                    },
+                  })}
                 />
                 {errors.lastName && (
-                  <p className="text-red-500" >{errors.lastName?.message}</p>
+                  <p className="text-red-500">{errors.lastName?.message}</p>
                 )}
               </div>
               <div className="form-control">
@@ -140,26 +144,50 @@ const Register = () => {
                   })}
                 />
                 {errors.email && (
-                  <p className="text-red-500" >{errors.email.message}</p>
+                  <p className="text-red-500">{errors.email.message}</p>
                 )}
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Password</span>
                 </label>
-                <input
-                  type="password"
+                
+                <div className="relative ">
+                  {/**
+                   * set icon here 
+                   *  */ }
+                  <span className="absolute inset-y-0 right-4 flex items-center pl-2">
+                    <button
+                      type="button"
+                      title="search"
+                      className="p-1 focus:outline-none focus:ring"
+                    >
+                      {
+                        showPass ? (
+                          <IoEye
+                            onClick={() => setShowPass(!showPass)}
+                          />
+                        ) : (
+                          <IoEyeOff
+                            onClick={() => setShowPass(!showPass)}
+                          />
+                        )
+                      }
+                    </button>
+                  </span>
+                  <input
+                  type={showPass ? "text" : "password"}
                   placeholder="password"
-                  className="input input-bordered"
+                  className="input input-bordered w-full py-2 pl-10 text-sm rounded-md  focus:outline-none dark:bg-gray-100 dark:text-gray-800 focus:dark:bg-gray-50 focus:dark:border-violet-600"
                   {...register("password", {
                     required: {
                       value: true,
                       message: "password field is required",
                     },
-                   minLength: {
-                     value: 6,
-                     message: "password must be at least 6 characters",
-                   },
+                    minLength: {
+                      value: 6,
+                      message: "password must be at least 6 characters",
+                    },
                     pattern: {
                       value: /^(?=.*[a-z])(?=.*[A-Z]).*$/,
                       message:
@@ -168,8 +196,9 @@ const Register = () => {
                   })}
                 />
                 {errors.password && (
-                  <p className="text-red-500" >{errors.password.message}</p>
+                  <p className="text-red-500">{errors.password.message}</p>
                 )}
+                </div>
               </div>
               <div className="form-control mt-6">
                 <button className="btn btn-primary">Register</button>
@@ -181,7 +210,7 @@ const Register = () => {
                 Login
               </Link>{" "}
             </p>
-            {regError && <p className="text-red-500" >{regError}</p>}
+            {regError && <p className="text-red-500">{regError}</p>}
           </div>
         </div>
       </div>
