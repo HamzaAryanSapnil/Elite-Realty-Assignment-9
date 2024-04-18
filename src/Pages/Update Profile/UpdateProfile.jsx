96cf32eea83ce25ade75815c27887d7fc9969c4e
HamzaAryanSapnil <139946462+HamzaAryanSapnil@users.noreply.github.com>
2024-04-18 18:18:37 +0600

    updateprofile done

--- src/Pages/Update Profile/UpdateProfile.jsx
+++ src/Pages/Update Profile/UpdateProfile.jsx
@@ -3,7 +3,7 @@ import { AuthContext } from "../Provider/AuthProvider";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import updateVideo from "../../assets/updateGif.gif";
-import updateBg from '../../assets/cool-background.png'
+import updateBg from "../../assets/cool-background.png";
 
 const UpdateProfile = () => {
   const { user, updateUserProfile } = useContext(AuthContext);
@@ -13,11 +13,10 @@ const UpdateProfile = () => {
     handleSubmit,
     register,
     formState: { errors },
-    
   } = useForm({
     defaultValues: {
       name: user?.displayName,
-      photoUrl: user?.photoURL,
+      photoUrl: user?.photoURL ? user.photoURL : "https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" ,
     },
   });
 
@@ -31,63 +30,68 @@ const UpdateProfile = () => {
       })
       .catch((error) => console.log(error));
   };
+
   return (
     <div>
       {/* <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 30 30">
 <path d="M 15.003906 3 C 8.3749062 3 3 8.373 3 15 C 3 21.627 8.3749062 27 15.003906 27 C 25.013906 27 27.269078 17.707 26.330078 13 L 25 13 L 22.732422 13 L 15 13 L 15 17 L 22.738281 17 C 21.848702 20.448251 18.725955 23 15 23 C 10.582 23 7 19.418 7 15 C 7 10.582 10.582 7 15 7 C 17.009 7 18.839141 7.74575 20.244141 8.96875 L 23.085938 6.1289062 C 20.951937 4.1849063 18.116906 3 15.003906 3 z"></path>
 </svg> */}
-      <div className="hero min-h-screen bg-base-200" 
-       style={{backgroundImage: `url(${updateBg})`, backgroundSize: "cover", backgroundPosition: "center"}}>
-        <div className="absolute w-full h-full top-20 left-0 bg-black opacity-35" ></div>
-        <div className="hero-content flex-col lg:flex-row-reverse bg-base-300 rounded-xl" 
-       
-        >
-        <img src={updateVideo} className="max-w-sm rounded-lg shadow-2xl" />
-  
+      <div className="hero min-h-screen bg-base-200 font-manrope"
+      style={{backgroundImage: `url(${updateBg})`, backgroundSize: "cover", backgroundPosition: "center"}}
+      >
+        <div className="absolute w-full h-full top-20 left-0 bg-black opacity-35"></div>
+        <div className="hero-content flex-col lg:flex-row-reverse bg-base-300 rounded-xl">
+          <img src={updateVideo} className="max-w-sm rounded-lg shadow-2xl" />
+
           <div>
             <h1 className="text-5xl font-bold">Hello {user.displayName}</h1>
-            <p className="py-6">You can just update your name and photourl</p>
+            <p className="py-6 text-descolor70">You can just update your name and photourl</p>
           </div>
           <form onSubmit={handleSubmit(onSubmit)} className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Name</span>
-                </label>
-                <input
-                  type="name"
-                  placeholder="name"
-                  className="input input-bordered"
-                  {...register("name", {
-                    required: {
-                      value: true,
-                      message: "name field is required",
-                    }
-                  })}
-                  
-                />
-                {errors.name && <p className="text-red-600" >{errors.name?.message}</p>}
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">PhotoUrl</span>
-                </label>
-                <input
-                  {...register("photoUrl", {
-                    required: {
-                      value: true,
-                      message: "photoUrl field is required",
-                    }
-                  })}
-                  placeholder="photoUrl"
-                  className="input input-bordered"
-                  type="text"
-                />
-                {errors.photoUrl && <p className="text-red-600" >{errors.photoUrl?.message}</p>}
-              </div>
-              <div className="form-control mt-6">
-                <button className="btn btn-primary animate__pulse">Update Profile</button>
-              </div>
-            </form>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="name"
+                placeholder="name"
+                className="input input-bordered"
+                {...register("name", {
+                  required: {
+                    value: true,
+                    message: "name field is required",
+                  },
+                })}
+              />
+              {errors.name && (
+                <p className="text-red-600">{errors.name?.message}</p>
+              )}
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">PhotoUrl</span>
+              </label>
+              <input
+                {...register("photoUrl", {
+                  required: {
+                    value: true,
+                    message: "photoUrl field is required",
+                  },
+                })}
+                placeholder="photoUrl"
+                className="input input-bordered"
+                type="text"
+              />
+              {errors.photoUrl && (
+                <p className="text-red-600">{errors.photoUrl?.message}</p>
+              )}
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary animate__pulse">
+                Update Profile
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
--- src/Pages/User Profile/UserProfile.jsx
+++ src/Pages/User Profile/UserProfile.jsx
@@ -1,46 +1,100 @@
 import { useContext } from "react";
 import { AuthContext } from "../Provider/AuthProvider";
 import { Link } from "react-router-dom";
-import uesrbg from '../../assets/userbg.mp4'
+import uesrbg from "../../assets/userbg.mp4";
 const UserProfile = () => {
   const { user } = useContext(AuthContext);
   return (
     <div>
-    <div className="hero min-h-screen bg-base-200">
-      <div className="absolute w-full h-full top-20 left-0 bg-black opacity-60" ></div>
-      <video src={uesrbg} autoPlay loop muted className="w-full h-full object-cover"></video>
-  <div className="hero-content flex-col lg:flex-row bg-base-200">
-    <img src={user?.photoURL ? user?.photoURL : "https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" }className="max-w-sm rounded-lg shadow-2xl" />
-    <div>
-      <h1 className="text-5xl font-bold ">Elite Realty</h1>
-      <p className="py-6 ">Hello {user?.displayName}. Here&apos;s your user profile info. You can modify your name and your photourl if you want</p>
-    </div>
-    <fieldset className="grid grid-cols-4 gap-6 p-6 rounded-md  dark:bg-gray-50">
-			<div className="space-y-2 col-span-full lg:col-span-1">
-				<p className="font-medium ">Profile</p>
-				<p className="text-xs ">{user.displayName}</p>
-			</div>
-			<div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3 card bg-white p-4">
-				<div className="col-span-full sm:col-span-3">
-					<label htmlFor="username" className="text-sm">Username</label>
-					<input id="username" type="text" placeholder="Username" className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" value={user.displayName} disabled/>
-				</div>
-				<div className="col-span-full sm:col-span-3">
-					<label htmlFor="website" className="text-sm">Your Photo Url</label>
-					<input id="website" type="text" placeholder="https://" className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" value={user?.photoURL} disabled/>
-				</div>
-				
-				<div className="col-span-full">
-					<label htmlFor="bio" className="text-sm">Photo</label>
-					<div className="flex items-center space-x-2">
-						<img src={ user?.photoURL ? user?.photoURL : "https://source.unsplash.com/30x30/?random"} alt="" className="w-10 h-10 dark:bg-gray-500 rounded-full dark:bg-gray-300" />
-						<Link to="/update_profile" ><button type="button" className=" btn btn-info btn-outline px-4 py-2 border rounded-md dark:border-gray-800">Change</button></Link>
-					</div>
-				</div>
-			</div>
-		</fieldset>
-  </div>
-</div>
+      <div className="hero min-h-screen bg-base-200 font-cabin">
+        <div className="absolute w-full h-full top-20 left-0 bg-black opacity-60"></div>
+        <video
+          src={uesrbg}
+          autoPlay
+          loop
+          muted
+          className="w-full h-full object-cover"
+        ></video>
+		
+        <div className="hero-content flex-col lg:flex-row bg-base-200">
+          <img
+            src={
+              user?.photoURL
+                ? user?.photoURL
+                : "https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
+            }
+            className="max-w-sm rounded-lg shadow-2xl"
+          />
+          <div>
+            <h1 className="text-5xl font-bold text-signBtn">Elite Realty</h1>
+            <p className="py-6 ">
+              Hello {user?.displayName}. Here&apos;s your user profile info. You
+              can modify your name and your photourl if you want
+            </p>
+          </div>
+          <fieldset className="grid grid-cols-4 gap-6 p-6 rounded-md  dark:bg-gray-50">
+            <div className="space-y-2 col-span-full lg:col-span-1">
+              <p className="font-medium text-cardDescolor">Profile</p>
+              <p className="text-2xl font-bold font-titillium text-descolor">
+                {user.displayName}
+              </p>
+            </div>
+            <div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3 card bg-white p-4">
+              <div className="col-span-full sm:col-span-3">
+                <label htmlFor="username" className="text-sm text-descolor70">
+                  Username
+                </label>
+                <input
+                  id="username"
+                  type="text"
+                  placeholder="Username"
+                  className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300"
+                  value={user.displayName}
+                  disabled
+                />
+              </div>
+              <div className="col-span-full sm:col-span-3">
+                <label htmlFor="website" className="text-sm text-descolor70">
+                  Your Photo Url
+                </label>
+                <input
+                  id="website"
+                  type="text"
+                  placeholder="https://"
+                  className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300"
+                  value={user?.photoURL}
+                  disabled
+                />
+              </div>
+
+              <div className="col-span-full font-manrope text-cardDescolor">
+                <label htmlFor="bio" className="text-sm">
+                  Photo
+                </label>
+                <div className="flex items-center space-x-2">
+                  <img
+                    src={
+                      user?.photoURL
+                        ? user?.photoURL
+                        : "https://source.unsplash.com/30x30/?random"
+                    }
+                    alt=""
+                    className="w-10 h-10 dark:bg-gray-500 rounded-full dark:bg-gray-300"
+                  />
+                  <Link to="/update_profile">
+                    <button
+                      type="button"
+                      className=" btn btn-info btn-outline px-4 py-2 border rounded-md dark:border-gray-800"
+                    >
+                      Change
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </fieldset>
+        </div>
+      </div>
     </div>
   );
 };
