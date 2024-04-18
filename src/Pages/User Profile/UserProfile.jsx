9b0fda76456963504b1a3c388646a8184b814994
HamzaAryanSapnil <139946462+HamzaAryanSapnil@users.noreply.github.com>
2024-04-18 08:28:43 +0600

    user profile done

--- src/Pages/User Profile/UserProfile.jsx
+++ src/Pages/User Profile/UserProfile.jsx
@@ -4,117 +4,40 @@ import { Link } from "react-router-dom";
 const UserProfile = () => {
   const { user } = useContext(AuthContext);
   return (
-    <div  >
-      
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row">
-          <img
-            src={
-              user?.photoURL
-                ? user?.photoURL
-                : "https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
-            }
-            className="max-w-sm rounded-lg shadow-2xl"
-          />
-          <div>
-            <h1 className="text-5xl font-bold mb-4 ">
-              {user?.displayName ? user?.displayName : "User"}
-            </h1>
-            <p className="py-6">Here is Your Profile Info</p>
-
-            <div>
-              <div className="overflow-x-auto">
-                <table className="table">
-                  {/* head */}
-                  <thead>
-                    <tr>
-                      <th>Name</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {/* row 1 */}
-                    <tr>
-                      <td>
-                        <div className="flex items-center gap-3">
-                          <div className="avatar">
-                            <div className="mask mask-squircle w-12 h-12">
-                              <img
-                                src={
-                                  user.photoURL
-                                    ? user.photoURL
-                                    : "https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
-                                }
-                                alt="Avatar Tailwind CSS Component"
-                              />
-                            </div>
-                          </div>
-                          <div>
-                            <div className="font-bold">{user.displayName}</div>
-                            <div className="text-sm opacity-50">Bangladesh</div>
-                          </div>
-                        </div>
-                      </td>
-                    </tr>
-                    {/* row 2 */}
-                    <tr>
-                      <td>
-                        <div className="flex items-center gap-3">
-                          <div>
-                            <div className="font-bold">Your PhotoUrl</div>
-                          </div>
-                        </div>
-                      </td>
-                    </tr>
-                    {/* row 3 */}
-                    <tr>
-                      <td>
-                        <div className="flex items-center gap-3">
-                          <div>
-                            <div className="font-bold">
-                              {user.photoURL
-                                ? user.photoURL
-                                : "https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"}
-                            </div>
-                          </div>
-                        </div>
-                      </td>
-                    </tr>
-                    {/* row 4 */}
-                    <tr>
-                      <td>
-                        <div className="flex items-center gap-3">
-                          <div>
-                            <div className="font-bold">Your Email</div>
-                          </div>
-                        </div>
-                      </td>
-                    </tr>
-                    {/* row 5 */}
-                    <tr>
-                      <td>
-                        <div className="flex items-center gap-3">
-                          <div>
-                            <div className="font-bold">{user.email}</div>
-                          </div>
-                        </div>
-                      </td>
-                    </tr>
-                  </tbody>
-                  {/* foot */}
-                </table>
-              </div>
-            </div>
-            <p className="text-sm mb-4">
-              Do you want to update your profile, please click here
-            </p>
-            <Link to="/update_profile">
-              <button className="btn btn-primary animate__shakeX animate__delay-5s animate__slower">
-                Update
-              </button>
-            </Link>
-          </div>
-        </div>
-      </div>
+    <div>
+    <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content flex-col lg:flex-row">
+    <img src={user?.photoURL ? user?.photoURL : "https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" }className="max-w-sm rounded-lg shadow-2xl" />
+    <div>
+      <h1 className="text-5xl font-bold">Elite Realty</h1>
+      <p className="py-6">Hello {user?.displayName}. Here&apos;s your user profile info. You can modify your name and your photourl if you want</p>
+    </div>
+    <fieldset className="grid grid-cols-4 gap-6 p-6 rounded-md shadow-lg dark:bg-gray-50">
+			<div className="space-y-2 col-span-full lg:col-span-1">
+				<p className="font-medium">Profile</p>
+				<p className="text-xs">{user.displayName}</p>
+			</div>
+			<div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
+				<div className="col-span-full sm:col-span-3">
+					<label htmlFor="username" className="text-sm">Username</label>
+					<input id="username" type="text" placeholder="Username" className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" value={user.displayName} disabled/>
+				</div>
+				<div className="col-span-full sm:col-span-3">
+					<label htmlFor="website" className="text-sm">Your Photo Url</label>
+					<input id="website" type="text" placeholder="https://" className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" value={user?.photoURL} disabled/>
+				</div>
+				
+				<div className="col-span-full">
+					<label htmlFor="bio" className="text-sm">Photo</label>
+					<div className="flex items-center space-x-2">
+						<img src={ user?.photoURL ? user?.photoURL : "https://source.unsplash.com/30x30/?random"} alt="" className="w-10 h-10 dark:bg-gray-500 rounded-full dark:bg-gray-300" />
+						<Link to="/update_profile" ><button type="button" className=" btn btn-info btn-outline px-4 py-2 border rounded-md dark:border-gray-800">Change</button></Link>
+					</div>
+				</div>
+			</div>
+		</fieldset>
+  </div>
+</div>
     </div>
   );
 };
