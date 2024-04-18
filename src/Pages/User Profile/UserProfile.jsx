2c5927ba59a402dd95e801f39ea7ae1dd796f5ba
HamzaAryanSapnil <139946462+HamzaAryanSapnil@users.noreply.github.com>
2024-04-18 10:03:12 +0600

    user profile card done

--- src/Pages/User Profile/UserProfile.jsx
+++ src/Pages/User Profile/UserProfile.jsx
@@ -9,16 +9,16 @@ const UserProfile = () => {
     <div className="hero min-h-screen bg-base-200">
       <div className="absolute w-full h-full top-20 left-0 bg-black opacity-60" ></div>
       <video src={uesrbg} autoPlay loop muted className="w-full h-full object-cover"></video>
-  <div className="hero-content flex-col lg:flex-row">
+  <div className="hero-content flex-col lg:flex-row bg-base-200">
     <img src={user?.photoURL ? user?.photoURL : "https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" }className="max-w-sm rounded-lg shadow-2xl" />
     <div>
-      <h1 className="text-5xl font-bold text-white">Elite Realty</h1>
-      <p className="py-6 text-white">Hello {user?.displayName}. Here&apos;s your user profile info. You can modify your name and your photourl if you want</p>
+      <h1 className="text-5xl font-bold ">Elite Realty</h1>
+      <p className="py-6 ">Hello {user?.displayName}. Here&apos;s your user profile info. You can modify your name and your photourl if you want</p>
     </div>
-    <fieldset className="grid grid-cols-4 gap-6 p-6 rounded-md shadow-lg dark:bg-gray-50">
+    <fieldset className="grid grid-cols-4 gap-6 p-6 rounded-md  dark:bg-gray-50">
 			<div className="space-y-2 col-span-full lg:col-span-1">
-				<p className="font-medium text-white">Profile</p>
-				<p className="text-xs text-white">{user.displayName}</p>
+				<p className="font-medium ">Profile</p>
+				<p className="text-xs ">{user.displayName}</p>
 			</div>
 			<div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3 card bg-white p-4">
 				<div className="col-span-full sm:col-span-3">
