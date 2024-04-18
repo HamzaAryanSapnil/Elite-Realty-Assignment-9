092e79d97546e780ac261486b633ca2495281b3e
HamzaAryanSapnil <139946462+HamzaAryanSapnil@users.noreply.github.com>
2024-04-18 19:20:14 +0600

    login section complete

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -3,7 +3,9 @@ import { useForm } from "react-hook-form";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../Provider/AuthProvider";
 import { toast } from "react-toastify";
-
+import loginGif from "../../assets/3d login.gif";
+import googleIcon from '../../assets/3d-fluency-google-logo.png'
+import githubIcon from '../../assets/github.png'
 const Login = () => {
   const [loginError, setLoginError] = useState("");
   const { logIn, googleLogin, githubLogin } = useContext(AuthContext);
@@ -62,9 +64,89 @@ const Login = () => {
   };
   console.log(watch("example"));
   return (
-    <div>
-      <div className="hero min-h-screen  bg-base-200">
-        <div className="hero-content  flex-col ">
+    <div className="" >
+      <div className="hero flex justify-around items-center min-h-screen  bg-base-200">
+        <div className="md:hidden hero-content  flex-col bg-cover bg-no-repeat bg-center "
+        style={{
+          backgroundImage: `linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)),url(${loginGif})`,
+        }}
+        >
+          <div className="text-center ">
+            <h1 className="text-5xl font-bold text-white opacity-80">Login now!</h1>
+          </div>
+          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form onSubmit={handleSubmit(onSubmit)} className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  {...register("email", {
+                    required: {
+                      value: true,
+                      message: "email field is required",
+                    },
+                    pattern: {
+                      value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                      message: "invalid email address",
+                    },
+                  })}
+                />
+                {errors.email && <p className="text-red-600" >{errors.email?.message}</p>}
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  {...register("password", {
+                    required: {
+                      value: true,
+                      message: "password field is required",
+                    },
+                    minLength: {
+                      value: 6,
+                      message: "password must be at least 6 characters",
+                    },
+                  })}
+                  placeholder="password"
+                  className="input input-bordered"
+                  type="password"
+                />
+                {errors.password && <p className="text-red-600" >{errors.password?.message}</p>}
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-outline   text-signBtn animate__pulse ">Login</button>
+              </div>
+            </form>
+
+            {loginError && <p className="text-red-600">{loginError}</p>}
+            <div className="text-center">
+              <p className="text-center ">Or Login with</p>
+              <div className="flex justify-center items-center mt-2 gap-x-2">
+                <button onClick={handleGoogleLogin} className="btn">
+                  <i><img src={googleIcon} className="w-10 h-10" alt=""  /></i>
+                </button>
+
+                <button onClick={handleGithubLogin} className="btn">
+                  <i><img src={githubIcon}  alt="" /></i>
+                </button>
+              </div>
+            </div>
+
+            <p className="text-center">
+              Donot have any account?
+              <Link to="/register" className="text-orange-600 font-bold">
+                {" "}
+                Register
+              </Link>
+            </p>
+          </div>
+        </div>
+        <div className="hidden md:hero-content  flex-col ">
           <div className="text-center ">
             <h1 className="text-5xl font-bold">Login now!</h1>
           </div>
@@ -113,7 +195,7 @@ const Login = () => {
                 {errors.password && <p className="text-red-600" >{errors.password?.message}</p>}
               </div>
               <div className="form-control mt-6">
-                <button className="btn btn-primary animate__pulse">Login</button>
+                <button className="  text-signBtn btn btn-outline animate__pulse ">Login</button>
               </div>
             </form>
 
@@ -121,12 +203,12 @@ const Login = () => {
             <div className="text-center">
               <p className="text-center ">Or Login with</p>
               <div className="flex justify-center items-center mt-2 gap-x-2">
-                <button onClick={handleGoogleLogin} className="btn btn-primary">
-                  Google
+              <button onClick={handleGoogleLogin} className="btn">
+                  <i><img src={googleIcon} className="w-10 h-10" alt=""  /></i>
                 </button>
 
-                <button onClick={handleGithubLogin} className="btn btn-primary">
-                  Github
+                <button onClick={handleGithubLogin} className="btn">
+                  <i><img src={githubIcon}  alt="" /></i>
                 </button>
               </div>
             </div>
@@ -140,7 +222,16 @@ const Login = () => {
             </p>
           </div>
         </div>
+        <div className="md:block hidden" >
+      <div className="card w-96 bg-base-100 shadow-xl">
+  
+  <div className="card-body border-2 border-signBtn shadow-md shadow-signBtn">
+  <figure><img src={loginGif} alt="Shoes" /></figure>
+  </div>
+</div>
       </div>
+      </div>
+      
     </div>
   );
 };
